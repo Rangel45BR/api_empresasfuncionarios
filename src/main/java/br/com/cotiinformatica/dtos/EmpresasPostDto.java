e9004d56caa0ee5a7f558906259d97a034561978
Rangel45BR <christiano.rangel@hotmail.com>
2022-11-10 11:42:57 -0300

    first commit

---
+++ src/main/java/br/com/cotiinformatica/dtos/EmpresasPostDto.java
@@ -0,0 +1,21 @@
+package br.com.cotiinformatica.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+
+public class EmpresasPostDto {
+
+	private String nomeFantasia;
+	private String razaoSocial;
+	private String cnpj;
+	
+}
